0c6ca429f3520ca8b104077fec5eed0c6dbdc00a
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2024-08-22 18:13:46 +0200

    fix renderlayer

--- src/main/java/smartin/cubiccart/client/CubiccartsClient.java
+++ src/main/java/smartin/cubiccart/client/CubiccartsClient.java
@@ -1,10 +1,13 @@
 package smartin.cubiccart.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import smartin.cubiccart.Cubiccarts;
 
 public class CubiccartsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), Cubiccarts.COPPER_RAIL);
     }
 }
